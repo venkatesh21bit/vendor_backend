2f87d9d332992565b2cf1fda3a1d33b4ab854690
venkatesh21bit <venkatesh.k21062005@gmail.com>
2025-06-29 15:07:26 +0530

    invoice unique constraint set to false

--- app/models.py
+++ app/models.py
@@ -179,7 +179,7 @@ class OrderItem(models.Model):
         return f"{self.product.name} x {self.quantity} (Order {self.order.order_id})"
     
 class Invoice(models.Model):
-    invoice_number = models.CharField(max_length=20)
+    invoice_number = models.CharField(max_length=20, unique=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="invoices")
     Retailer = models.ForeignKey(Retailer, on_delete=models.CASCADE)
     invoice_date = models.DateTimeField()
